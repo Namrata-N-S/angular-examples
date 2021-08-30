af4525fa5b232d7afe1511d0f59e1bf18603d72a
Namrata-N-S <namrata2607@gmail.com>
2021-08-30 14:01:51 +0530

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { UpdateBackgroundPipe } from './update-background.pipe';
+import { ToDoListComponent } from './to-do-list/to-do-list.component';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, UpdateBackgroundPipe],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    UpdateBackgroundPipe,
+    ToDoListComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
